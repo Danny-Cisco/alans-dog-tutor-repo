0dbc4adbd5e9c734da0b8814840e1b7a0bd44f96
Danny-Cisco <gumballelectronics@gmail.com>
2023-06-28 17:15:47 +1000

    test deploy for vercel

--- scripts/openai-client.js
+++ scripts/openai-client.js
@@ -1,10 +1,14 @@
+let myPrompt = "Write a short story about a dog on an adventure.";
+
 fetch('/api/openai', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ prompt: myPrompt }),
-  })
-  .then(response => response.json())
-  .then(data => console.log(data.text));
-  
+})
+.then(response => response.json())
+.then(data => console.log(data.text))
+.catch((error) => {
+    console.error('Error:', error);
+});
