bfde2bfa7afaa849f4a2a1dc88e61cf95be8da94
Danny-Cisco <gumballelectronics@gmail.com>
2023-06-28 21:40:14 +1000

    debug choices further

--- api/openai.js
+++ api/openai.js
@@ -22,27 +22,39 @@ module.exports = async (req, res) => {
 import { Configuration, OpenAIApi } from 'openai';
 
 const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
 export default async (req, res) => {
     try {
-      const { prompt } = req.body;
-  
-      const response = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 7,
-        temperature: 0,
-      });
-      console.log(response);
-      res.status(200).json({ text: response.choices[0].text });
+        const { prompt } = req.body;
+
+        const response = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            max_tokens: 7,
+            temperature: 0,
+        });
+        console.log('Response:', response);
+        console.log('Choices:', response.choices);
+
+        if (response.choices && response.choices.length > 0) {
+            console.log('Text:', response.choices[0].text);
+            res.status(200).json({ text: response.choices[0].text });
+        } else {
+            console.error('No choices found in the response.');
+            res.status(500).json({ error: 'No choices found in the response.' });
+        }
+        res.status(200).json({ text: response.choices[0].text });
     } catch (error) {
-      console.error('Error:', error);  // Log the error
-      res.status(500).json({ error: error.toString() });
+        console.error('Error:', error);  // Log the error
+        res.status(500).json({ error: error.toString() });
     }
 };
+
+
+
 
 
 
