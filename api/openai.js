56decc45cf3045ba16054802a6c3ac052d942f27
Danny-Cisco <gumballelectronics@gmail.com>
2023-06-28 21:55:19 +1000

    add middleware express json

--- api/openai.js
+++ api/openai.js
@@ -1,44 +1,25 @@
-/*const { OpenAIApi } = require("openai");
-
-module.exports = async (req, res) => {
-  const openai = new OpenAIApi({ apiKey: process.env.OPENAI_API_KEY });
-  const prompt = req.body.prompt;
-
-  try {
-    const result = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt,
-      max_tokens:4000
-    });
-
-    res.status(200).json({ text: result.data.choices[0].text });
-  } catch (error) {
-    console.error('OpenAI API call failed:', error);
-    res.status(500).json({ error: error.toString() });
-  }
-};
-*/
-
+import express from 'express';
 import { Configuration, OpenAIApi } from 'openai';
 
+const app = express();
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-export default async (req, res) => {
+app.use(express.json());
+
+app.post('/api/openai', async (req, res) => {
     try {
         console.log('Request body:', req.body);
         const { prompt } = req.body;
 
         const response = await openai.createCompletion({
-            model: "text-davinci-003",
+            model: 'text-davinci-003',
             prompt: prompt,
             max_tokens: 7,
             temperature: 0,
         });
-
-        
 
         console.log('Response:', response);
         console.log('Choices:', response.choices);
@@ -50,15 +31,15 @@ export default async (req, res) => {
             console.error('No choices found in the response.');
             res.status(500).json({ error: 'No choices found in the response.' });
         }
-        res.status(200).json({ text: response.choices[0].text });
     } catch (error) {
-        console.error('Error:', error);  // Log the error
+        console.error('Error:', error);
         res.status(500).json({ error: error.toString() });
     }
-};
+});
 
-
-
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
 
 
 
