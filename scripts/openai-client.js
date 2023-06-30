a33aa84640de0f492bd1a761b828d0f5a2fc4605
Danny-Cisco <gumballelectronics@gmail.com>
2023-06-30 22:21:05 +1000

    prompt clearing

--- scripts/openai-client.js
+++ scripts/openai-client.js
@@ -5,22 +5,30 @@ document.getElementById('myForm').addEventListener('submit', function(e) {
   // Get the prompt from the input box
   let myPrompt = document.getElementById('prompt-input-box').value;
 
+  // Clear the prompt input box
+  document.getElementById('prompt-input-box').value = '';
+
+  // Get the 'response-box' element
+  let responseBox = document.getElementById('response-box');
+
+  // Place the question in the 'response-box'
+  responseBox.innerHTML = 'Q: ' + myPrompt + '<br><br>';
+
   // Use fetch to send the POST request as before
   fetch('/api/openai', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ prompt: myPrompt }),
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ prompt: myPrompt }),
   })
   .then(response => response.json())
   .then(data => {
-      // Get the 'response-box' element
-      let responseBox = document.getElementById('response-box');
-      // Update the text inside the 'response-box' element with the API response
-      responseBox.textContent = data.text;
+    // Update the text inside the 'response-box' element with the API response
+    responseBox.innerHTML += 'A: ' + data.text;
   })
   .catch((error) => {
-      console.error('Error:', error);
+    console.error('Error:', error);
   });
 });
+
