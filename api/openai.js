b8af38221590095ed67d86934a71a17977091fc2
Danny-Cisco <gumballelectronics@gmail.com>
2023-06-30 14:47:22 +1000

    change response-completion

--- api/openai.js
+++ api/openai.js
@@ -11,27 +11,20 @@ app.use(express.json());
 
 app.post('/api/openai', async (req, res) => {
     try {
-        console.log('Request body:', req.body);
+        
         const { prompt } = req.body;
-        console.log('Prompt:', prompt);
+        
 
-        const response = await openai.createCompletion({
+        const completion = await openai.createCompletion({
             model: 'text-davinci-003',
             prompt: prompt,
             max_tokens: 7,
             temperature: 0,
         });
 
-        console.log('Response:', response);
-        console.log('Choices:', response.choices);
-
-        if (response.choices && response.choices.length > 0) {
-            console.log('Text:', response.choices[0].text);
-            res.status(200).json({ text: response.choices[0].text });
-        } else {
-            console.error('No choices found in the response.');
-            res.status(500).json({ error: 'No choices found in the response.' });
-        }
+            res.status(200).json({ text: completion.data.choices[0].text });
+        
+    
     } catch (error) {
         console.error('Error:', error);
         res.status(500).json({ error: error.toString() });
