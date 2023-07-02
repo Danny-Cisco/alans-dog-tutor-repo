88c8affe2db4bb9e3afa5840f997705c81d4a36b
Danny-Cisco <gumballelectronics@gmail.com>
2023-07-02 18:59:41 +1000

    use axios, return binary

--- api/narakeet.js
+++ api/narakeet.js
@@ -1,30 +1,28 @@
-import dotenv from 'dotenv';
-import {pipeline} from 'node:stream/promises';
-import {createWriteStream} from 'node:fs';
-import { Readable } from 'stream';
-import got from 'got';
-
+const axios = require('axios');
+const dotenv = require('dotenv');
 dotenv.config();
 
 module.exports = async (req, res) => {
     const APIKEY = process.env.NARAKEET_API_KEY;
-    const voice = 'mickey';
+    const voice = 'mike';
     const text = req.body.text;
 
-    await pipeline(
-        Readable.from([text]),
-        got.stream.post(
-            `https://api.narakeet.com/text-to-speech/m4a?voice=${voice}`,
-            {
-                headers: {
-                    'accept': 'application/octet-stream',
-                    'x-api-key': APIKEY,
-                    'content-type': 'text/plain'
-                }
+    try {
+        const response = await axios({
+            method: 'post',
+            url: `https://api.narakeet.com/text-to-speech/mp3?voice=${voice}`,
+            headers: {
+                'accept': 'application/octet-stream',
+                'x-api-key': APIKEY,
+                'content-type': 'text/plain'
             },
-        ),
-        createWriteStream('result.m4a')
-    );
+            data: text,
+            responseType: 'arraybuffer'
+        });
 
-    res.status(200).send('Audio created successfully');
+        res.setHeader('Content-Type', 'audio/mpeg');
+        res.send(Buffer.from(response.data, 'binary'));
+    } catch (error) {
+        res.status(500).send(`Server error: ${error.message}`);
+    }
 }
